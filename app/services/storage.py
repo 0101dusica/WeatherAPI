f27061990a224630d9ea90c44e9f7f46199e0a82
0101dusica <trbovicdusica@gmail.com>
2025-06-10 18:58:43 +0200

    Implement S3 data storage

--- app/services/storage.py
+++ app/services/storage.py
@@ -1,7 +1,6 @@
 import json
 import os
 import aiobotocore.session
-from datetime import datetime
 from app.utils.config import settings
 
 # Stores weather data locally or in S3, depending on configuration.
@@ -12,13 +11,19 @@ async def store_weather_data(weather_data: dict) -> str:
         city = weather_data["city"]
         timestamp = weather_data["timestamp"].replace(":", "-")
         filename = f"{city}_{timestamp}.json"
-        
+
         # Check environment setting for local storage
-        if os.getenv("USE_LOCAL_STORAGE"):
+        use_local = settings.USE_LOCAL_STORAGE == "1"
+        if use_local:
             os.makedirs("weather_data", exist_ok=True)
             with open(f"weather_data/{filename}", "w") as f:
                 json.dump(weather_data, f)
             return f"weather_data/{filename}" # Returning local path
         
         # Upload data to S3
-        pass
+        await s3_client.put_object(
+            Bucket=settings.S3_BUCKET,
+            Key=filename,
+            Body=json.dumps(weather_data)
+        )
+        return f"s3://{settings.S3_BUCKET}/{filename}" # Returning S3 URL
