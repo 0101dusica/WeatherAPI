c26118180fc4ff15825543ec00845655b647d972
0101dusica <trbovicdusica@gmail.com>
2025-06-10 18:56:32 +0200

    Added AWS configuration to config file

--- app/utils/config.py
+++ app/utils/config.py
@@ -5,5 +5,8 @@ load_dotenv()
 
 class Settings:
     OPENWEATHERMAP_API_KEY = os.getenv("OPENWEATHERMAP_API_KEY")
+    USE_LOCAL_STORAGE= os.getenv("USE_LOCAL_STORAGE")
+    S3_BUCKET = os.getenv("S3_BUCKET")
+    DYNAMODB_TABLE = os.getenv("DYNAMODB_TABLE")
 
 settings = Settings()
