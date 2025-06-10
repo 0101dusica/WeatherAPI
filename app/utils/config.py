1a7c1d37d70ecd467d9d965e8750e03ea5000760
0101dusica <trbovicdusica@gmail.com>
2025-06-10 19:51:30 +0200

    Add AWS region configuration

--- app/utils/config.py
+++ app/utils/config.py
@@ -6,6 +6,7 @@ load_dotenv()
 class Settings:
     OPENWEATHERMAP_API_KEY = os.getenv("OPENWEATHERMAP_API_KEY")
     USE_LOCAL_STORAGE= os.getenv("USE_LOCAL_STORAGE")
+    AWS_REGION = os.getenv("AWS_REGION", "us-east-1")
     S3_BUCKET = os.getenv("S3_BUCKET")
     DYNAMODB_TABLE = os.getenv("DYNAMODB_TABLE")
 
